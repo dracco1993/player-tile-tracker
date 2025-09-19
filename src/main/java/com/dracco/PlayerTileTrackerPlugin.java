7c6fb98587750307bf53d0a7c974642405ad46ee
dracco1993 <dracco1993@gmail.com>
2025-09-19 00:00:45 -0400

    More renaming

--- src/main/java/com/dracco/PlayerTileTrackerPlugin.java
+++ src/main/java/com/dracco/PlayerTileTrackerPlugin.java
@@ -13,15 +13,19 @@ import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
 
 @Slf4j
-@PluginDescriptor(name = "PlayerTileTracker")
-public class PlayerTileTracker extends Plugin {
+@PluginDescriptor(name = "Player Tile Tracker")
+public class PlayerTileTrackerPlugin extends Plugin {
 	@Inject
 	private Client client;
 
 	@Inject
 	private PlayerTileTrackerConfig config;
+
+	@Inject
+	private OverlayManager overlayManager;
 
 	@Override
 	protected void startUp() throws Exception {
@@ -36,7 +40,8 @@ public class PlayerTileTracker extends Plugin {
 	@Subscribe
 	public void onGameStateChanged(GameStateChanged gameStateChanged) {
 		if (gameStateChanged.getGameState() == GameState.LOGGED_IN) {
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "PlayerTileTracker says " + config.greeting(), null);
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "PlayerTileTracker says hello " + config.targetPlayer(),
+					null);
 		}
 	}
 
--- src/test/java/com/dracco/PlayerTileTrackerTest.java
+++ src/test/java/com/dracco/PlayerTileTrackerTest.java
@@ -1,14 +1,12 @@
 package com.dracco;
 
-import com.dracco.PlayerTileTracker;
-
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
 public class PlayerTileTrackerTest {
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws Exception {
-		ExternalPluginManager.loadBuiltin(PlayerTileTracker.class);
+		ExternalPluginManager.loadBuiltin(PlayerTileTrackerPlugin.class);
 		RuneLite.main(args);
 	}
 }
